16ff28fea0ff816d89d3ce1ea77705ac31f78106
CSE023-27 <2023nitsgr176@nitsri.ac.in>
2025-07-07 12:20:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
-app.get('/forecast', (req, res) => {
-    res.sendFile(path.join(__dirname,'app', 'indexs.html'));
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
